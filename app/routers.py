9d7a9fcbd405aca88cdd8d11fc399883cf3188fd
Kocherga <malinovnamasana@gmail.com>
2025-01-30 13:12:35 +0300

    basic get_weather handler implemented

--- app/routers.py
+++ app/routers.py
@@ -1,13 +1,32 @@
+import aiohttp
+import json
+
 from aiogram import Router
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 
+from app.config import settings
+from app.api import fetch_weather_data
 
 router = Router()
 
 
 @router.message(CommandStart())
-async def start_command(message: Message):
+async def start_handler(message: Message):
     await message.reply(
         f"Здравствуй. Я буду отправлять информацию о погоде в указанном тобой городе в то время, которое ты хочешь."
     )
+
+
+@router.message(Command("get_weather"))
+async def get_weather_handler(message: Message):
+    city = "Нижнекамск"
+    token = settings.WEATHER_API_TOKEN
+
+    try:
+        weather = await fetch_weather_data(token, city)
+        dict_str = json.dumps(weather, ensure_ascii=False, indent=4)
+        await message.answer(dict_str)
+    except aiohttp.ClientError as error:
+        print(error)
+        await message.reply(f"Ошибка при получении данных: {error}")
