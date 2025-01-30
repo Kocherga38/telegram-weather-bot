95676f3402231e49014aac40e2eaf800800934da
Kocherga <malinovnamasana@gmail.com>
2025-01-30 13:09:09 +0300

    basic version of getting weather data added

---
+++ app/api.py
@@ -0,0 +1,36 @@
+import asyncio
+import aiohttp
+
+from app.config import settings
+
+from pprint import pprint
+
+
+async def fetch_weather_data(token: str, city: str) -> dict:
+    url = "https://api.openweathermap.org/data/2.5/weather"
+    params = {"q": city, "appid": token, "units": "metric"}
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url=url, params=params, timeout=5) as response:
+            response.raise_for_status()
+            return await response.json()
+
+
+async def get_weather(token: str, city: str) -> dict:
+    try:
+        return await fetch_weather_data(token, city)
+
+    except aiohttp.ClientError as error:
+        print("Error: ", error)
+
+
+async def main():
+    city = input("Введите город: ").strip() or "Нижнекамск"
+    token = settings.WEATHER_API_TOKEN
+    weather = await get_weather(token=token, city=city)
+    if weather:
+        pprint(weather)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
