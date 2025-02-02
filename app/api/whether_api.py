daa9432992cd65d67d8041bcc5363e183f27e06d
Kocherga <malinovnamasana@gmail.com>
2025-02-02 18:52:47 +0300

    Переделал взаимодействие с апи, убрав ненужное

--- app/api/whether_api.py
+++ app/api/whether_api.py
@@ -1,33 +1,29 @@
 import asyncio
+
 import aiohttp
 
 from app.config.bot import bot_settings as settings
 
 from pprint import pprint
 
-
 async def fetch_weather_data(token: str, city: str) -> dict:
     url = "https://api.openweathermap.org/data/2.5/weather"
     params = {"q": city, "appid": token, "units": "metric"}
 
     async with aiohttp.ClientSession() as session:
-        async with session.get(url=url, params=params, timeout=5) as response:
-            response.raise_for_status()
-            return await response.json()
-
-
-async def get_weather(token: str, city: str) -> dict:
-    try:
-        return await fetch_weather_data(token, city)
-
-    except aiohttp.ClientError as error:
-        print("Error: ", error)
-
+        try:
+            async with session.get(url=url, params=params, timeout=5) as response:
+                response.raise_for_status()
+                return await response.json()
+        except aiohttp.ClientResponseError as error:
+            return {"code": error.status, "message": error.message}
+        except aiohttp.ClientError as error:
+            return {"cod": "500", "message": f"Ошибка при соединении: {str(error)}"}
 
 async def main():
-    city = input("Введите город: ").strip() or "Нижнекамск"
+    city = input("Enter city: ").strip() or "Нижнекамск"
     token = settings.WEATHER_API_TOKEN
-    weather = await get_weather(token=token, city=city)
+    weather = await fetch_weather_data(token=token, city=city)
     if weather:
         pprint(weather)
 
