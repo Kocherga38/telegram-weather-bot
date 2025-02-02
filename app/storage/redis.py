d34ba7a2993efe782db68f4f77d0146f60fd58cf
Kocherga <malinovnamasana@gmail.com>
2025-02-02 17:30:22 +0300

    feat: added Redis integration

---
+++ app/storage/redis.py
@@ -0,0 +1,27 @@
+import logging
+import redis.asyncio as aioredis
+
+from app.config.redis import redis_settings as settings
+
+logger = logging.getLogger(__name__)
+
+async def get_redis():
+    return await aioredis.from_url(
+        f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}", decode_responses=True
+    )
+
+async def save_city_in_redis(user_id: int, city: str) -> None:
+    redis = await get_redis()
+    await redis.set(f"user:{user_id}:city", city)
+
+    logger.info(f"Saved in Redis.\nUser id: {user_id}\nCity: {city}")
+    await redis.aclose()
+
+
+async def get_city(user_id: int) -> str:
+    redis = await get_redis()
+    city = await redis.get(f"user:{user_id}:city")
+    if city is None:
+        logger.info(f"No city found for user {user_id}.")
+    await redis.aclose()
+    return city
